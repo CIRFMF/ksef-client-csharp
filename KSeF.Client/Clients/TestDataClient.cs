0a55c4d060e4bf97e51919536fb4fd0689126ba7
Sebastian Podjasek <sebastian.podjasek@intelliway.pl>
2026-04-30 16:22:23 +0200

    chore: whitespace cleanup

--- KSeF.Client/Clients/TestDataClient.cs
+++ KSeF.Client/Clients/TestDataClient.cs
@@ -72,12 +72,12 @@ namespace KSeF.Client.Clients
              ExecuteAsync<Status>(Routes.TestData.ProductionRateLimits, HttpMethod.Post, accessToken, cancellationToken);
 
         /// <inheritdoc />
-        public Task<Status> UnblockContextAsync(ContextIdentifier requestPayload, string accessToken, CancellationToken cancellationToken = default) =>  
+        public Task<Status> UnblockContextAsync(ContextIdentifier requestPayload, string accessToken, CancellationToken cancellationToken = default) =>
             ExecuteAsync<Status, ContextIdentifier>(Routes.TestData.UnblockContext, requestPayload, accessToken, cancellationToken);
 
         /// <inheritdoc />
-        public Task<Status> BlockContextAsync(ContextIdentifier requestPayload, string accessToken, CancellationToken cancellationToken = default) =>            
-            ExecuteAsync<Status, ContextIdentifier>(Routes.TestData.BlockContext, requestPayload, accessToken, cancellationToken);        
+        public Task<Status> BlockContextAsync(ContextIdentifier requestPayload, string accessToken, CancellationToken cancellationToken = default) =>
+            ExecuteAsync<Status, ContextIdentifier>(Routes.TestData.BlockContext, requestPayload, accessToken, cancellationToken);
 
     }
 }
