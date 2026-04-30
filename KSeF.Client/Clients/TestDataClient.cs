ed4eb350e7f0998f8b4d50728672359f0a3d45d8
Sebastian Podjasek <sebastian.podjasek@intelliway.pl>
2026-04-30 16:21:44 +0200

    fix: TestDataClient method for setting rate limit as in production environment
    Fixes #216

--- KSeF.Client/Clients/TestDataClient.cs
+++ KSeF.Client/Clients/TestDataClient.cs
@@ -69,7 +69,7 @@ namespace KSeF.Client.Clients
 
         /// <inheritdoc />
         public Task<Status> RestoreProductionRateLimitsAsync(string accessToken, CancellationToken cancellationToken = default) =>
-             ExecuteAsync<Status>(Routes.TestData.RestoreDefaultCertificatesLimitInCurrentSubject, HttpMethod.Delete, accessToken, cancellationToken);
+             ExecuteAsync<Status>(Routes.TestData.ProductionRateLimits, HttpMethod.Post, accessToken, cancellationToken);
 
         /// <inheritdoc />
         public Task<Status> UnblockContextAsync(ContextIdentifier requestPayload, string accessToken, CancellationToken cancellationToken = default) =>  
